8b6ebaeb5480c6f996f11f4c6e83623f49ce41e4
lvyitao <lvyitao521@163.com>
2020-06-05 14:31:36 +0900

    搭建项目框架

---
+++ msr-edu/src/main/java/com/msr/edu/EduApplication.java
@@ -0,0 +1,22 @@
+package com.msr.edu;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author lv
+ * @version V1.0
+ * @Package com.msr.edu
+ * @date 2020/6/5 14:17
+ * @Copyright © 株式会社多言语系统研究所
+ */
+
+//主类  或启动类
+@SpringBootApplication
+@MapperScan("com.msr.edu.mapper")
+public class EduApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(EduApplication.class,args);
+    }
+}
